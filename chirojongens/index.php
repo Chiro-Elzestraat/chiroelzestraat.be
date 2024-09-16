5e95504ba3b1cfc413c8b548a1e57870fc696330
Jasper Neyens <Jasper.Neyens@student.ucll.be>
2024-09-16 17:23:11 +0200

    't program sep - dec toegevoegd

--- chirojongens/index.php
+++ chirojongens/index.php
@@ -355,6 +355,7 @@ $_SESSION['r'] = $r;
           <h2 class="text-uppercase ftco-uppercase">'t Program</h2>
           <div class="row justify-content-center mb-5">
             <div class="col-md-7">
+              <p class="lead"><a href="https://tprogram.chiroelzestraat.be/Program-sept-dec2024.pdf">'t Program september - december 2024</a></p>
               <p class="lead"><a href="https://tprogram.chiroelzestraat.be/Kampboekje2024.pdf">Kampboekje 2024</a></p>
               <p class="lead"><a href="https://tprogram.chiroelzestraat.be/Program-april-juni-2024.pdf">'t Program
                 april - juni 2024</a></p>
